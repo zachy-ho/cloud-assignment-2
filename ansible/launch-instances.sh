36e23517f4f6b86d86ddf5cbd877b8f61e7d260b
Zhuolun Wu <zhuolunw@student.unimelb.edu.au>
2021-05-04 11:23:32 +1000

    fixed typos

--- ansible/launch-instances.sh
+++ ansible/launch-instances.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. ./unimelb-comp90024-2021-grp-49-openrc.sh; ansible-playbook launch_instances.yaml
+. ./unimelb-comp90024-2021-grp-49-openrc.sh; ansible-playbook launch-instances.yaml
