ae897cc438f0b1147a1abc7921e3ef928c487f06
Zhuolun Wu <zhuolunw@student.unimelb.edu.au>
2021-05-08 14:05:17 +1000

    Hosts file and SSH key

--- ansible/setup-environment.sh
+++ ansible/setup-environment.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-. ./unimelb-comp90024-2021-grp-49-openrc.sh; ansible-playbook setup-environment.yaml -i inventory/hosts.ini
+export ANSIBLE_HOST_KEY_CHEKCING=False
+. ./unimelb-comp90024-2021-grp-49-openrc.sh; ansible-playbook setup-environment.yaml -i inventory/hosts
