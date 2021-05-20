291557b5465aa901c12880e2643d3c8117150f17
PeAt2333 <69140669+PeAt2333@users.noreply.github.com>
2021-05-20 14:40:31 +1000

    Update creds.py

--- harvester/creds.py
+++ harvester/creds.py
@@ -7,7 +7,7 @@
 # consumer_secret="AwgyemcsnEYGV2OI50ptsYNKQm0zgo8j96n7Sfyh2xtIFnWleh"
 
 # Jiashuai's token
-access_token="1392070254939938820-HFBYQN3QOHROsclJ5XhstU8zYv6Pgg"
-access_token_secret="AAm6nkGty1fwMt1l2SMeEBhLA1tzj8wPS2HX2iC7eMAuf"
-consumer_key="BxalJdaSoG9teOOmCLUG0Lorf"
-consumer_secret="FCyEfwME3sgydCiUvmkr50IoursJ8lr6IfsW7NDVXpFuKtWqg2"
+access_token="1392070254939938820-T2j1eDHA5xCWXnRyuPVXit1GmraAAv"
+access_token_secret="pQivfZLy7MJkn7vLEWCc5AvpeBmq3uAi6KYC26xepEBhy"
+consumer_key="n6nv4y3zmLlZkJ9StgNZOYNWI"
+consumer_secret="YHsLc6ZpYpwVk6OgObicraOmgsIo24P5C5CEilz6uNFHdLwaQK"
