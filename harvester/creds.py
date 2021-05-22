2c022714f88a4776d84e211068bbc2e5f8a61cca
Zhuolun Wu <zhuolunw@student.unimelb.edu.au>
2021-05-22 13:47:10 +1000

    Update creds.py

--- harvester/creds.py
+++ harvester/creds.py
@@ -1,13 +1,13 @@
 #contains the user creds
 
 # Vignesh's token
-# access_token="1389053699012448256-i6DOTUDOKaGmWzJ1TpONQd1gQYyJdz"
-# access_token_secret="lKGF6LO0niG3oUiPEhQVHA7qaPRav7b30mkFRnEfYJUjH"
-# consumer_key="ZZlUEJQpRTv1i2QUyGGCMny1L"
-# consumer_secret="AwgyemcsnEYGV2OI50ptsYNKQm0zgo8j96n7Sfyh2xtIFnWleh"
+access_token="1389053699012448256-i6DOTUDOKaGmWzJ1TpONQd1gQYyJdz"
+access_token_secret="lKGF6LO0niG3oUiPEhQVHA7qaPRav7b30mkFRnEfYJUjH"
+consumer_key="ZZlUEJQpRTv1i2QUyGGCMny1L"
+consumer_secret="AwgyemcsnEYGV2OI50ptsYNKQm0zgo8j96n7Sfyh2xtIFnWleh"
 
-# Jiashuai's token
-access_token="1392070254939938820-T2j1eDHA5xCWXnRyuPVXit1GmraAAv"
-access_token_secret="pQivfZLy7MJkn7vLEWCc5AvpeBmq3uAi6KYC26xepEBhy"
-consumer_key="n6nv4y3zmLlZkJ9StgNZOYNWI"
-consumer_secret="YHsLc6ZpYpwVk6OgObicraOmgsIo24P5C5CEilz6uNFHdLwaQK"
+# # Jiashuai's token
+# access_token="1392070254939938820-T2j1eDHA5xCWXnRyuPVXit1GmraAAv"
+# access_token_secret="pQivfZLy7MJkn7vLEWCc5AvpeBmq3uAi6KYC26xepEBhy"
+# consumer_key="n6nv4y3zmLlZkJ9StgNZOYNWI"
+# consumer_secret="YHsLc6ZpYpwVk6OgObicraOmgsIo24P5C5CEilz6uNFHdLwaQK"
